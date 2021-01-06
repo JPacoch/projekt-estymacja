a63a4054434689e7ef04edc1d7c5d70f48ee5e51
JPacoch <pacochp@gmail.com>
2021-01-06 19:50:18 +0100

    hm

--- Smiech-Pacocha.R
+++ Smiech-Pacocha.R
@@ -7,14 +7,16 @@ library(gstat)
 library(tmap)
 library(fields)
 library(rsample)
+library(rgeos)
 #library(tidyverse)
 #Wczytanie danych wejsciowych
 pomiary = read_sf("dane/train.gpkg")
 siatka = read_stars("dane/pusta_siatka.tif")
 lc = read_stars("dane/lc.tif")
 elev = read_stars("dane/elev.tif")
-lc = as.factor(lc)
+#lc = as.factor(lc)
 lc_legend = read_sf("dane/lc_legend.xls", encoding = "UTF-8")
+boundary = read_sf("dane/granica.shp")
 
 #nadanie informacji o ukladzie 
 siatka = st_set_crs(siatka, value = 2180)
@@ -23,10 +25,8 @@ pomiary = st_set_crs(pomiary, value = 2180)
 #przygotowanie palety kolorow
 palette = hcl.colors(12, palette = "Temps")
 
-
 #wizualizacja  rozlozenia punktow pomiarowych
 #na tle siatki (m. Poznan)
-
 plot(siatka, reset = FALSE)
 plot((pomiary), add = TRUE)
 #plot((elev), add = TRUE)
@@ -35,6 +35,7 @@ tm_shape(siatka) +
   tm_raster(legend.show = FALSE) +
 tm_shape(pomiary) + tm_symbols(col = 'red')
 tmap_mode('view')
+
 #eksploracjna analiza danych
 
 median(pomiary$PM10)
@@ -45,7 +46,7 @@ max(pomiary$PM10)
 
 sd(pomiary$PM10)
 
-ggplot(pomiary,aes(PM10)) + geom_histogram() #2 wartosci odstajace -40 i 0
+ggplot(pomiary,aes(PM10)) + geom_histogram() #2 wartosci odstajace
 #-40 i 0 to wartosci bledne, nie jest mozliwe ujemne lub zerowe stezenie
 #pylow PM10
 
@@ -59,6 +60,7 @@ summary(pomiary$PM10)
 ggplot(pomiary, aes(y = PM10)) +
   scale_x_discrete()+ geom_boxplot() #zbiór charakteryzuje sie 
 #wystepowaniem sporej liczby wartosci odstajacych
+
 
 #WAŻNE
 #
@@ -100,15 +102,34 @@ ggplot(pomiary, aes(y = PM10)) +
 #ggplot(pomiary_clean, aes(y = PM10)) +
 #  scale_x_discrete() + geom_boxplot()
 
+#Przestrzenna analiza danych
+#Miary relacji przestrzennych
+hscat(PM10 ~ 1, data = pomiary, breaks = seq(0, 12000, by = 1500))
+
+covario = variogram(PM10 ~ 1, locations = pomiary,
+                    covariogram = TRUE)
+plot(covario)
+
 vario_cloud = variogram(PM10 ~ 1, locations = pomiary, 
                         cloud = TRUE)
 plot(vario_cloud)
 
+#semiwariogram
+0.5 * nrow(pomiary) * (nrow(pomiary) - 1) #liczba par obserwacji
+
+area_sample = st_area(boundary) / nrow(pomiary)
+sqrt(area_sample) #srednia odleglosc miedzy punktami
+
+area_poz = st_area(boundary)
+0.5 * sqrt(area_poz)
+
 vario = variogram(PM10 ~ 1, locations = pomiary,
-                      cutoff = 18000, width = 1200, map = FALSE)
+                      cutoff = 8088)
+plot(vario)
+
 
 vario_map = variogram(PM10 ~ 1, locations = pomiary,
-                  cutoff = 18000, width = 1200, map = TRUE)
+                  cutoff = 8088, width = 1000, map = TRUE)
 plot(vario_map, 
      col.regions = hcl.colors(40, palette = "ag_GrnYl", rev = TRUE)) 
 #zjawisko nie wykazuje anizotropii
@@ -129,6 +150,11 @@ model_zl = vgm(4, "Gau", 1600,
 fitted_gausph = fit.variogram(vario, model_zl)
 plot(vario, model = fitted_gausph)
 
+model_zl2 = vgm(12, "Sph", 3200, 
+                add.to = vgm(1, model = "Bes",
+                             range = 1700, nugget = 11.5))
+fitted_gausph2 = fit.variogram(vario, model_zl2)
+plot(vario, model = fitted_gausph2)
 
 set.seed(351)
 pomiary_split = initial_split(pomiary, prop = 0.75, strata = PM10)
@@ -136,18 +162,19 @@ train = training(pomiary_split)
 test = testing(pomiary_split)
 
 vario_train = variogram(PM10 ~ 1, locations = train,
-                  cutoff = 18000, width = 1200, map = FALSE)
+                  cutoff = 8088, map = FALSE)
 
 plot(vario_train)
 fitted_train = fit.variogram(vario_train, model)
 plot(vario_train, model = fitted_gaunug)
 
 #metoda krigingu prostego 
+mean(pomiary$PM10)
 test_sk = krige(PM10 ~ 1, 
                 locations = train,
                 newdata = test,
-                model = fitted_gausph,
-                beta = 15)
+                model = fitted_gausph2,
+                beta = 33)
 fault_sk = test$PM10 - test_sk$var1.pred
 summary(fault_sk)
 RMSE_sk = sqrt(mean((test$PM10 - test_sk$var1.pred) ^ 2))
@@ -162,20 +189,20 @@ ggplot(test_sk, aes(var1.pred, test$PM10)) +
 test_ok = krige(PM10 ~ 1,
            locations = train,
            newdata = test, 
-           model = fitted_gausph, 
+           model = fitted_gausph2, 
            nmax = 14)
 fault_ok = test$PM10 - test_ok$var1.pred
 summary(fault_ok)
 RMSE_ok = sqrt(mean((test$PM10 - test_ok$var1.pred) ^ 2))
 RMSE_ok
-write.csv(RMSE_ok, file = 'Smiech_Pacocha.csv',
-          row.names = FALSE)
+#write.csv(RMSE_ok, file = 'Smiech_Pacocha.csv',
+ #         row.names = FALSE)
 
 
 ok = krige(PM10 ~ 1,
                 locations = pomiary,
                 newdata = siatka, 
-                model = fitted_gausph, 
+                model = fitted_gausph2, 
                 nmax = 16)
 plot(ok["var1.pred"], col = palette)
 ggplot(test_ok, aes(var1.pred, test$PM10)) +
@@ -207,7 +234,7 @@ plot(vario_kzt, fitted_kzt)
 test_kzt = krige(PM10 ~ 1, 
             locations = trainkzt, 
             newdata = test, 
-            model = fitted_gausph)
+            model = fitted_gausph2)
 summary(test_kzt)
 
 fault_kzt = test$PM10 - test_kzt$var1.pred
@@ -228,3 +255,4 @@ plot(elev)
 idw_pomiary = idw(PM10 ~ 1, locations = pomiary,
                  newdata = siatka, idp = 2)
 plot(idw_pomiary["var1.pred"], main = "IDW", col = palette)
+
